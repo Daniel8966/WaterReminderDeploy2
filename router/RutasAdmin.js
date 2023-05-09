4c7a82a66cedf8d60602fd6adb031b73c4c8b8ef
user <pulido.robles.daniel.alfonso@gmail.com>
2023-05-08 23:55:46 -0600

    administrador usuarios

--- router/RutasAdmin.js
+++ router/RutasAdmin.js
@@ -1,12 +1,21 @@
 
-import express from 'express';
+import express, { query } from 'express';
 const router = express.Router();
+import {connection} from '../database/DatabaseConexion.js'
+
 
 
 //------RUTAS DE LAS PAGINAS------
-router.get('/', (req, res) => {
+router.get('/',  async (req, res) => {
     if (req.session.loggedin && req.session.admin) {
-        res.render('administrador')
+        const query = `select idUsuario, Usuario, email from usuario;`
+        connection.query( query , async (err, respuesta, fields) => {
+            if (err) return console.log("Error", err);
+
+            res.render('administrador', {usuarios : respuesta} )
+
+      })
+        
     }
 
 })
