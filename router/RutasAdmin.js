a81d70a2116e21ccb91906198155ab2a086eac5a
user <pulido.robles.daniel.alfonso@gmail.com>
2023-05-09 13:56:51 -0600

    admin sesion auth middleware

--- router/RutasAdmin.js
+++ router/RutasAdmin.js
@@ -4,9 +4,23 @@ const router = express.Router();
 import {connection} from '../database/DatabaseConexion.js'
 import manageSession from './sesiones.js';
 
+function sesionAdmin(nombre){  
+    return function( req, res, next) {
+        if (req.session.admin){
+            console.log('Sesion de admin en ' + nombre )
+            next();
+        }else{
+            res.redirect('/login') 
+            console.log('no hay sesion activa como admin')
+            
+        }
+
+    }
+}
+
 
 //------RUTAS DE LAS PAGINAS------
-router.get('/', manageSession('admin usuarios'), async (req, res) => {
+router.get('/', manageSession('admin usuarios'), sesionAdmin('admin usuarios'),  async (req, res) => {
     if (req.session.admin) {
         const query = `select idUsuario, Usuario, email from usuario;`
         connection.query( query , async (err, respuesta, fields) => {
--- router/sesiones.js
+++ router/sesiones.js
@@ -1,16 +1,14 @@
 // function manageSession(req, res, next, titulo) {
 
 // }
-function manageSession(viewName) {
+function manageSession(titulo) {
   return function (req, res, next) {
     if (req.session.loggedin) {
-      console.log('Sesion existente - ' + viewName)
+      console.log('Sesion existente - ' + titulo)
       next();
     } else {
-      console.log('NO hay sesion activa regresando en: ' + viewName)
+      console.log('NO hay sesion activa regresando en: ' + titulo)
       res.redirect('/login', 404, {
-        login: false,
-        name: 'Inicie Sesion'
       })
     }
   }
