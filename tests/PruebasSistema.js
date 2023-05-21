a6bbad1d1be361bc4908d3b3f64651b2a5f5f140
user <pulido.robles.daniel.alfonso@gmail.com>
2023-05-20 22:22:11 -0600

    prueba Sistema Principal

--- tests/PruebasSistema.js
+++ tests/PruebasSistema.js
@@ -8,7 +8,7 @@ import axios from 'axios'
 
 router.get('/', sesionAdmin('admin pruebas'), manageSession('admin pruebas'), async (req, res) => {
 
-    
+
     res.render('pruebas', { mensaje: false })
 
 
@@ -29,7 +29,7 @@ function sesionAdmin(nombre) {
     }
 }
 
-router.get('/systemStatus',  async (req, res) => {
+router.get('/systemStatus', async (req, res) => {
     res.status(200).send('En línea');
 })
 
@@ -40,14 +40,14 @@ async function verificarEstado() {
         const response = await axios.get('http://localhost:3150/pruebas/systemStatus');
         if (response.status === 200 && response.data === 'En línea') {
             console.log('El proyecto está en linea');
-            status = `El proyecto esta en linea`;
+            return status = `El proyecto esta en linea`;
         } else {
             console.log('El proyecto no esta en linea');
-            status = `El proyecto no esta en linea`;
+            return status = `El proyecto no esta en linea`;
         }
     } catch (error) {
         console.error('Error al verificar el estado del proyecto', error);
-    
+
         return error;
 
     }
@@ -58,17 +58,32 @@ async function verificarEstado() {
 
 
 router.get('/checkStatus', sesionAdmin('admin pruebas'), manageSession('admin pruebas'), async (req, res) => {
-    verificarEstado();
+    let status = await verificarEstado();
     const host = 'localhost';
     ping.promise.probe(host, { port: PORT, timeout: 10 })
 
         .then((result) => {
             if (result.alive) {
-                console.log(`${host}:3150 está en línea`);
-                res.render('pruebas', { mensaje: `sistemas operando en puerto${PORT}` })
+              
+                let minTime = 'Tiempo mínimo:' + result.min;
+                let maxtime = 'Tiempo máximo:' + result.max;
+                let Promedio = 'Promedio de tiempo:' + result.avg;
+                let mensaje = `sistemas operando en ${host}:${PORT}`
+             
+
+
+                //Obtener informacion de los paquetes que se enviaron para hacer el ping con result.otuput y dividir el string
+                let packetInfo = result.output;
+                let packetInfoStr1 = packetInfo.split('para 127.0.0.1:')
+                let packetInfoStr2 = packetInfoStr1[1].split('Tiempos aproximados')
+
+                //mandar todo como un array 
+                let resultadosPrueba = [status,packetInfoStr2[0], minTime, maxtime, Promedio, mensaje]
+
+                res.render('pruebas', { mensaje: resultadosPrueba})
             } else {
                 console.log(`${host}:3150 está fuera de línea`);
-                res.render('pruebas', { mensaje: 'amogus' })
+                res.render('pruebas', { mensaje: 'sistema fuera de linea' })
             }
         })
         .catch((error) => {
