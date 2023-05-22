881726094d193f656274f965b7e1cbf21e196db1
user <pulido.robles.daniel.alfonso@gmail.com>
2023-05-21 21:18:08 -0600

    database name

--- configENV.js
+++ configENV.js
@@ -1,4 +1,4 @@
-export const DB_NAME = process.env.DB_NAME || 'waterreminder';
+export const DB_NAME = process.env.DB_DATABASE || 'waterreminder';
 export const DB_HOST = process.env.DB_HOST || 'localhost';
 export const DB_PASSWORD = process.env.DB_PASSWORD || 'n0m3l0';
 export const DB_PORT = process.env.DB_PORT || 3306;
